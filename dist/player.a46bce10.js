cf69e51d52290cbbf452c25ed1792506e5f07be1
Алексей Боклогов <kashira87@mail.ru>
2023-08-30 14:17:21 +0300

    init Parcel

---
+++ dist/player.a46bce10.js
@@ -0,0 +1,534 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"js/player.js":[function(require,module,exports) {
+new Vue({
+  el: "#app",
+  data: function data() {
+    return {
+      audio: null,
+      circleLeft: null,
+      barWidth: null,
+      duration: null,
+      currentTime: null,
+      isTimerPlaying: false,
+      top: '20vh;',
+      left: '20vw;',
+      tracks: [{
+        name: "Mekanın Sahibi",
+        artist: "Norm Ender",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
+        url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
+        favorited: false
+      }, {
+        name: "Everybody Knows",
+        artist: "Leonard Cohen",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+        url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
+        favorited: true
+      }, {
+        name: "Extreme Ways",
+        artist: "Moby",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
+        url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
+        favorited: false
+      }, {
+        name: "Butterflies",
+        artist: "Sia",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
+        url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
+        favorited: false
+      }, {
+        name: "The Final Victory",
+        artist: "Haggard",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
+        url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
+        favorited: true
+      }, {
+        name: "Genius ft. Sia, Diplo, Labrinth",
+        artist: "LSD",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
+        url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
+        favorited: false
+      }, {
+        name: "The Comeback Kid",
+        artist: "Lindi Ortega",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
+        url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
+        favorited: true
+      }, {
+        name: "Overdose",
+        artist: "Grandson",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
+        url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
+        favorited: false
+      }, {
+        name: "Rag'n'Bone Man",
+        artist: "Human",
+        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
+        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
+        url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
+        favorited: false
+      }],
+      currentTrack: null,
+      currentTrackIndex: 0,
+      transitionName: null
+    };
+  },
+  methods: {
+    play: function play() {
+      if (this.audio.paused) {
+        this.audio.play();
+        this.isTimerPlaying = true;
+      } else {
+        this.audio.pause();
+        this.isTimerPlaying = false;
+      }
+    },
+    mousedownPlayer: function mousedownPlayer(e) {
+      var _this = this;
+      var player = document.querySelector('.player');
+      var header = document.querySelector('.player__header');
+      var coords = this.getCoords(header);
+      console.log("🚀 ~ mousedownPlayer ~ coords:", coords);
+      var shiftX = e.pageX - coords.left;
+      var shiftY = e.pageY - coords.top;
+      console.log(shiftX, shiftY);
+      // передвинуть player под координаты курсора
+      // и сдвинуть на половину ширины/высоты для центрирования
+      var movePlayer = function movePlayer(e) {
+        _this.left = e.pageX - shiftX + 'px';
+        _this.top = e.pageY - shiftY + 'px';
+      };
+      movePlayer(e);
+
+      // перемещать по экрану
+      document.onmousemove = function (e) {
+        movePlayer(e);
+      };
+
+      // отследить окончание переноса
+      player.onmouseup = function () {
+        document.onmousemove = null;
+        player.onmouseup = null;
+      };
+
+      // player.ondragstart = () => {
+      //   return false;
+      // };
+    },
+    getCoords: function getCoords(elem) {
+      var box = elem.getBoundingClientRect();
+      // console.log("🚀 ~ getCoords ~ box:", box)
+      // console.log("🚀 ~ getCoords ~ pageYOffset:", pageYOffset)
+      return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+      };
+    },
+    generateTime: function generateTime() {
+      var width = 100 / this.audio.duration * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      var durmin = Math.floor(this.audio.duration / 60);
+      var dursec = Math.floor(this.audio.duration - durmin * 60);
+      var curmin = Math.floor(this.audio.currentTime / 60);
+      var cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
+    },
+    updateBar: function updateBar(x) {
+      var progress = this.$refs.progress;
+      var maxduration = this.audio.duration;
+      var position = x - progress.offsetLeft;
+      var percentage = 100 * position / progress.offsetWidth;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      if (percentage < 0) {
+        percentage = 0;
+      }
+      this.barWidth = percentage + "%";
+      this.circleLeft = percentage + "%";
+      this.audio.currentTime = maxduration * percentage / 100;
+      this.audio.play();
+    },
+    clickProgress: function clickProgress(e) {
+      this.isTimerPlaying = true;
+      this.audio.pause();
+      this.updateBar(e.pageX);
+    },
+    prevTrack: function prevTrack() {
+      this.transitionName = "scale-in";
+      this.isShowCover = false;
+      if (this.currentTrackIndex > 0) {
+        this.currentTrackIndex--;
+      } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    nextTrack: function nextTrack() {
+      this.transitionName = "scale-out";
+      this.isShowCover = false;
+      if (this.currentTrackIndex < this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    resetPlayer: function resetPlayer() {
+      var _this2 = this;
+      this.barWidth = 0;
+      this.circleLeft = 0;
+      this.audio.currentTime = 0;
+      this.audio.src = this.currentTrack.source;
+      setTimeout(function () {
+        if (_this2.isTimerPlaying) {
+          _this2.audio.play();
+        } else {
+          _this2.audio.pause();
+        }
+      }, 300);
+    },
+    favorite: function favorite() {
+      this.tracks[this.currentTrackIndex].favorited = !this.tracks[this.currentTrackIndex].favorited;
+    }
+  },
+  created: function created() {
+    var vm = this;
+    this.currentTrack = this.tracks[0];
+    this.audio = new Audio();
+    this.audio.src = this.currentTrack.source;
+    this.audio.ontimeupdate = function () {
+      vm.generateTime();
+    };
+    this.audio.onloadedmetadata = function () {
+      vm.generateTime();
+    };
+    this.audio.onended = function () {
+      vm.nextTrack();
+      this.isTimerPlaying = true;
+    };
+
+    // this is optional (for preload covers)
+    for (var index = 0; index < this.tracks.length; index++) {
+      var element = this.tracks[index];
+      var link = document.createElement('link');
+      link.rel = "prefetch";
+      link.href = element.cover;
+      link.as = "image";
+      document.head.appendChild(link);
+    }
+  }
+});
+},{}],"C:/Users/User/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49499" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["C:/Users/User/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/player.js"], null)
+//# sourceMappingURL=/player.a46bce10.js.map
